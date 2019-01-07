4dba13f3fb427d48c2a2202d92667e21b5614784
Dakie <ktaka0130@gmail.com>
2019-01-07 22:59:25 +0900

    Add One-pass algorithm

--- capture1/sample1.py
+++ capture1/sample1.py
@@ -1,5 +1,14 @@
 cap1 = ['F','F','B','B','B','F','B','B','B','F','F','B','F']
 cap2 = ['F','F','B','B','B','F','B','B','B','F','F','F','F']
+def pleaseConformOnepass(caps):
+    caps = caps + [caps[0]]
+    for i in range(1, len(caps)):
+        if caps[i] != caps[i-1]:
+            if caps[i] != caps[0]:
+                print('peple in positions', i, end='')
+            else:
+                print(' through', i, 'flip your caps!')
+
 def pleaseConform(caps):
     start = forward = backward = 0
     intervals = []
@@ -22,4 +31,7 @@ def pleaseConform(caps):
 
 if __name__ == '__main__':
     pleaseConform(cap1)
-    pleaseConform(cap2)
+    #pleaseConform(cap2)
+    print("")
+    pleaseConformOnepass(cap1)
+    #pleaseConformOnepass(cap2)
