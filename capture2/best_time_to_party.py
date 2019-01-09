861624e8575aa2e33258df96bfb859e0bbaaa86c
Dakie <ktaka0130@gmail.com>
2019-01-09 22:39:11 +0900

    Add bestTimeToPartySmart

--- capture2/best_time_to_party.py
+++ capture2/best_time_to_party.py
@@ -19,3 +19,35 @@ def celebrityDensity(schedule, start, end):
     return count
 
 bestTimeToParty(sched)
+
+sched2 = [(6.0, 8.0), (6.5, 12.0), (6.5, 7.0), (7.0, 8.0), (7.5, 10.0), (8.0, 9.0), (8.0, 10.0), (9.0, 12.0), (9.5, 10.0), (10.0, 11.0), (10.0, 12.0), (11.0, 12.0)]
+def bestTimeToPartySmart(schedule):
+    times = []
+    for c in schedule:
+        times.append((c[0], 'start'))
+        times.append((c[1], 'end'))
+    sortList(times)
+    maxcount, time = chooseTime(times)
+    print('Best time to attend the party is at', time, 'o\'clock', ':', maxcount, 'celebrities will be attending!')
+
+def sortList(tlist):
+    for index in range(0, len(tlist)-1):
+        index_min = index
+        for i in range(index, len(tlist)):
+            if tlist[index_min] > tlist[i]:
+                index_min = i
+        tlist[index], tlist[index_min] = tlist[index_min], tlist[index]
+
+def chooseTime(times):
+    count = maxcount = time = 0
+    for t in times:
+        if t[1] == 'start':
+            count += 1
+        elif t[1] == 'end':
+            count -= 1
+        if count > maxcount:
+            maxcount = count
+            time = t[0]
+    return maxcount, time
+
+bestTimeToPartySmart(sched2)
